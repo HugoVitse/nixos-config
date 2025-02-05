582f40cad4a218f530222a4d3856251f7fb7da7b
HugoVitse <hugovitse1@gmail.com>
2025-02-05 12:07:27 +0100

    update

---
+++ dev-shells/python/flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "python shell dev flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./python.nix { inherit pkgs; };
+        }
+      );
+}
--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 cp -r ./ags ~/.config/
 cp -r kitty.conf ~/.config/kitty
-cp .theme_mode ~/.cache/
 rm ./hardware-configuration.nix
 sudo cp /etc/nixos/hardware-configuration.nix .
 sudo nixos-rebuild switch --flake .
